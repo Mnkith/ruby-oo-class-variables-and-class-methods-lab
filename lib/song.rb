fc59e0e4243e39cd500e7c50a2b18ea582dac2c6
Mnkith <munkithabid@gmail.com>
2020-11-07 16:15:37 -0600

    commit tree 43e831d51fb39f5b2cbcf60e169ad52dc74fc45d

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+    @@artist_count[artist] = @@artists.count { | x | x == artist}
+    @@genre_count[genre] = @@genres.count { | x | x == genre}
+  end
+  attr_reader :name, :artist, :genre
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+end
